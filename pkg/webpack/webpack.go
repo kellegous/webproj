a0e4ef015728a88c45d7cba1cdea45bb36d93611
Kelly Norton <kellegous@gmail.com>
2022-03-13 13:32:34 -0400

    .

--- pkg/webpack/webpack.go
+++ pkg/webpack/webpack.go
@@ -1,12 +1,12 @@
 package webpack
 
 import (
-	"fmt"
+	"html/template"
 	"os"
 	"path/filepath"
 )
 
-const webpackCfgJS string = `
+const tsconfigJSON string = `
 {
 	"compilerOptions": {
 		"module": "commonjs",
@@ -22,7 +22,7 @@ const webpackCfgJS string = `
 		"downlevelIteration": true,
 		"paths": {
 			"*": [
-				"%s/*"
+				"{{.Src}}/*"
 			]
 		},
 		"lib": [
@@ -35,25 +35,104 @@ const webpackCfgJS string = `
 	},
 	"exclude": [
 		"node_modules",
-		"%s"
+		"{{.Dst}}"
 	],
 	"include": [
-		"./%s",
+		"./{{.Src}}",
 		"webpack.config.ts"
 	]
 }
 `
 
-const webpackCfgFile = "webpack.config.ts"
+const webpackCfgJS string = `
+import path from "path";
+import { Configuration, DefinePlugin } from "webpack";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
-func writeWebpackCfg(opts *Options) error {
-	w, err := os.Create(filepath.Join(opts.Root, webpackCfgFile))
+const webpackConfig = (): Configuration => ({
+	entry: {
+		"index": "./{{.Src}}/index.ts"
+	},
+	...(process.env.production || !process.env.development
+		? {}
+		: { devtool: "eval-source-map" }),
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+		plugins: [new TsconfigPathsPlugin({ configFile: "./tsconfig.json" })],
+	},
+	output: {
+		path: path.join(__dirname, "/{{.Dst}}"),
+		filename: "[name].js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: "ts-loader",
+				options: {
+					transpileOnly: true,
+				},
+				exclude: /{{.Dst}}/,
+			},
+			{
+				test: /\.s?css$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	// devServer: {
+	// 	port: 3000,
+	// 	open: true,
+	// 	historyApiFallback: true,
+	// },
+	plugins: [
+		// DefinePlugin allows you to create global constants which can be configured at compile time
+		new DefinePlugin({
+			"process.env": process.env.production || !process.env.development,
+		}),
+	],
+});
+
+export default webpackConfig;
+`
+
+const (
+	tsconfigFile      = "tsconfig.json"
+	webpackConfigFile = "webpack.config.ts"
+)
+
+func writeTSConfig(opts *Options) error {
+	t, err := template.New("text").Parse(tsconfigJSON)
+	if err != nil {
+		return err
+	}
+
+	w, err := os.Create(filepath.Join(opts.Root, tsconfigFile))
 	if err != nil {
 		return err
 	}
 	defer w.Close()
 
-	if _, err := fmt.Fprintf(w, webpackCfgJS, opts.Src, opts.Dst, opts.Src); err != nil {
+	if err := t.Execute(w, opts); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func writeWebpackConfig(opts *Options) error {
+	t, err := template.New("text").Parse(webpackCfgJS)
+	if err != nil {
+		return err
+	}
+
+	w, err := os.Create(filepath.Join(opts.Root, webpackConfigFile))
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	if err := t.Execute(w, opts); err != nil {
 		return err
 	}
 
@@ -68,8 +147,13 @@ type Options struct {
 }
 
 func CreateConfig(opts *Options) error {
-	if err := writeWebpackCfg(opts); err != nil {
+	if err := writeWebpackConfig(opts); err != nil {
 		return err
 	}
+
+	if err := writeTSConfig(opts); err != nil {
+		return err
+	}
+
 	return nil
 }
